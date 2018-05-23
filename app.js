fb227969f5ae3f775e9022aa2800215aeb592cae
Mayra Romero <nahytar@gmail.com>
2018-05-22 23:22:28 -0400

    revela texto oculto

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ window.onload = function () {
     var formName = document.getElementById("formName");
     var inputName = document.getElementById("inputName");
     var name = document.getElementById("name");
+
+    var btn = document.getElementById("button-change");
+    var text = document.getElementById("paragraph");
+    
     var formEmail = document.getElementById("formEmail");
     var inputEmail = document.getElementById("inputEmail");
     var email = document.getElementById("email");
@@ -10,10 +14,11 @@ window.onload = function () {
         name.innerHTML = inputName.value;
     });
 
+    btn.addEventListener("click", function () {
+        text.innerHTML = "<p>La metanfetamina de la serie son dulces</p>";
+    });
+
     formEmail.addEventListener("submit", function () {
             email.innerHTML = inputEmail.value;
     });
-    btn.addEventListener("click", function () {
-        text.innerHTML = "<h2>Git y Github te amamos</h2>";
-    })
 }
